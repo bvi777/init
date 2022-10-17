043e37de1d1b19964ffca6e522ae22cdaaa5aefb
bvi777 <64492266+bvi777@users.noreply.github.com>
2022-10-17 09:19:55 +0300

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo curl -s https://install.zerotier.com/ | sudo bash
+sudo systemctl zerotier-cli join 0cccb752f77952ff 
+sudo  systemctl enable zerotier-one > 
+sudo systemctl start zerotier-one > 
+
+# sudo curl -s https://install.zerotier.com/ | sudo bash > /dev/null 2>&1
+# sudo systemctl zerotier-cli join 0cccb752f77952ff > /dev/null 2>&1
+# sudo  systemctl enable zerotier-one > /dev/null 2>&1
+# sudo systemctl start zerotier-one > /dev/null 2>&1
